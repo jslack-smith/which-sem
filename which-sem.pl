ae7592d5017baabc627256df7f6dbea6c5738b0c
James Slack-Smith <jpslacksmith@hotmail.com.au>
2017-01-09 13:14:04 +1100

    Read courses into hash

--- which-sem.pl
+++ which-sem.pl
@@ -1,17 +1,40 @@
 #!/usr/bin/perl
 
-use warnings;
-use strict;
-
 # which-sem.pl
 # given a list of UNSW course codes, return when the courses run in table form
 
-#TODO get current year from computer
-my year = 2017;
+use warnings;
+use strict;
+
+use Data::Dumper;
+
+my $debug = 1;
+
+# TODO get current year from computer
+my $year = 2017;
+
+my %courses;
 
 # read course codes into courses hash
+while(my $code = <>) {
 
-# TODO check that courses exist, may not be neccesary
+    # convert to upper case
+    $code = uc $code;
+
+    # check code is legal, legal code is four letters followed by four numbers
+    # TODO check that course exists, may not be neccesary
+    if($code =~ /^\s*[A-Z]{4}\d{4}\s*$/) {
+        $courses{$code} = "";
+    } else {
+        # TODO better error handling
+        die "Illegal course code\n";
+    }
+}
+
+if($debug) {
+    print Dumper(\%courses);
+}
+
 
 
 
