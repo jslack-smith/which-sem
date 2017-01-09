fcb6678c6ae2b31b3b5fef798fc7ded25e549dfd
James Slack-Smith <jpslacksmith@hotmail.com.au>
2017-01-09 13:30:29 +1100

    Add test data files

--- which-sem.pl
+++ which-sem.pl
@@ -18,16 +18,17 @@ my %courses;
 # read course codes into courses hash
 while(my $code = <>) {
 
-    # convert to upper case
+    # clean code
+    chomp $code;
     $code = uc $code;
 
     # check code is legal, legal code is four letters followed by four numbers
     # TODO check that course exists, may not be neccesary
-    if($code =~ /^\s*[A-Z]{4}\d{4}\s*$/) {
-        $courses{$code} = "";
+    if((my $legal_code) = $code =~ /^\s*([A-Z]{4}\d{4})\s*$/) {
+        $courses{$legal_code} = "";
     } else {
         # TODO better error handling
-        die "Illegal course code\n";
+        print "Illegal course code: $code \n";
     }
 }
 
