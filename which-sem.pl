daa55b86866aaeb9b6e098058150d898aea1ea56
James Slack-Smith <jpslacksmith@hotmail.com.au>
2017-01-09 12:56:09 +1100

    Initial commit of which-sem.pl

---
+++ which-sem.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+# which-sem.pl
+# given a list of UNSW course codes, return when the courses run in table form
+
+#TODO get current year from computer
+my year = 2017;
+
+# read course codes into courses hash
+
+# TODO check that courses exist, may not be neccesary
+
+
+
+# update courses hash with when the courses are running
+
+
+
+# display courses in table
